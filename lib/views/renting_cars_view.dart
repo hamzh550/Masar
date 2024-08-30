c92c05ca1badc56216c738594f7cac69dda9d6aa
HAMZH ALZEQRI <hamzh550@gmail.com>
2024-08-30 17:58:16 +0100

    1ST PUSH

---
+++ lib/views/renting_cars_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RentingCarsView extends StatefulWidget {
+  const RentingCarsView({super.key});
+
+  @override
+  State<RentingCarsView> createState() => RentingCarsViewState();
+}
+
+class RentingCarsViewState extends State<RentingCarsView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Renting');
+  }
+}
---
+++ lib/views/shipping_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShippingView extends StatefulWidget {
+  const ShippingView({super.key});
+
+  @override
+  State<ShippingView> createState() => _ShippingViewState();
+}
+
+class _ShippingViewState extends State<ShippingView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Shipping');
+  }
+}
